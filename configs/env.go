208c0b9ab1bc637a5c3b1737bd6748907b0703cd
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2023-11-05 19:02:04 -0500

    Update env.go

--- configs/env.go
+++ configs/env.go
@@ -2,18 +2,17 @@ package configs
 
 import (
 	"DENV_Backend/models"
-	"os"
 )
 
 func GetPostgresProperties() (models.PostgresProperties, error) {
 	pgProperties := models.PostgresProperties{
-		Host:     os.Getenv("PGHOST"),
-		User:     os.Getenv("PGUSER"),
-		Password: os.Getenv("PGPASSWORD"),
-		DBName:   os.Getenv("PGDATABASE"),
-		Port:     os.Getenv("PGPORT"),
-		SSLMode:  os.Getenv("PGSSLMODE"),
-		TimeZone: os.Getenv("PGTZ"),
+		Host:     "localhost",
+		User:     "postgres",
+		Password: "postgres",
+		DBName:   "denv",
+		Port:     "5432",
+		SSLMode:  "disable",
+		TimeZone: "America/Lima",
 	}
 
 	return pgProperties, nil
